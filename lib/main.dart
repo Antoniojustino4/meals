559b05a7a88f3bde9642a6b4bdc919a9818b46a9
Antonio <antonio.justino@railmp.com>
2022-03-31 16:51:00 -0300

    TabBar and screen of details meals

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/utils/app_routes.dart';
 
+import 'screens/tabs_screen.dart';
 import 'screens/categories_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Vamos Cozinhar?',
       theme: ThemeData(
           fontFamily: 'Raleway',
           canvasColor: const Color.fromRGBO(255, 254, 229, 1),
@@ -21,12 +23,13 @@ class MyApp extends StatelessWidget {
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
               )),
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink,)
               .copyWith(secondary: Colors.amber)),
-      home: const CategoriesScreen(),
+      home: const TabsScreen(),
       routes: {
-        AppRoutes.HOME: (context) => const CategoriesScreen(),
+        //AppRoutes.HOME: (context) => const TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (context) => const CategoriesMealsScreen(),
+        AppRoutes.MEALS_DETAIL: (context) => const MealDetailScreen()
       },
     );
   }
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import 'package:meals/data/dummy_data.dart';
 
 import '../models/category.dart';
 
@@ -7,13 +9,21 @@ class CategoriesMealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     final category = ModalRoute.of(context)?.settings.arguments as Category;
+    final category = ModalRoute.of(context)?.settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
       ),
     );
   }
